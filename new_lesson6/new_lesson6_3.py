f2e7f25fa67f418127ae224becc07512d84d5a21
julia <zaytceva.jula@gmail.com>
2024-07-03 22:13:17 +0400

    new_lesson6

---
+++ new_lesson6/new_lesson6_3.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+waiting = WebDriverWait(driver, 40, 0.1)
+
+try:
+    driver.get(
+        'https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
+    waiting.until(EC.text_to_be_present_in_element((By.ID, "text"), 'Done!'))
+    get_attribute = driver.find_element(By.ID, "award").get_attribute("src")
+    print(get_attribute)
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.quit
+
+
+
